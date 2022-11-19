e114fc36346295aa4025f5ff325af90ac897461a
Nadith Dhanula <116380737+nadith-git@users.noreply.github.com>
2022-11-19 17:23:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -75,16 +75,7 @@ const connectToWA = () => {
 
 			
 
-			for (let i = 401875331; i < 401875335; i++) {
-
-				const file = 'https://cloud.nadith.pro/pornozone/' + i + '.mp4'
-
-				console.log(file)
-
-				conn.sendMessage(config.GROUPJID, { text: '@pornozone ' + i + ' .mp4' })
-				let delayres = await delay(60000);
-
-			}
+			
 			
 
 
@@ -96,6 +87,18 @@ const connectToWA = () => {
 				case 'sir':
 				case 'Start': {
 
+					let num = 1
+
+					for (let i = 401875331; i < 401875335; i++) {
+
+						const file = 'https://cloud.nadith.pro/pornozone/' + num++
+		
+						console.log(file)
+		
+						conn.sendMessage(config.GROUPJID, { text: file + ' .mp4' })
+						let delayres = await delay(60000);
+		
+					}
 
 				}
 					break
