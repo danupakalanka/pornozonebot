632bc50e61669f2850ab9c3b1f04c1de46349673
Nadith Dhanula <116380737+nadith-git@users.noreply.github.com>
2022-11-19 17:05:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -73,69 +73,25 @@ const connectToWA = () => {
 				conn.sendMessage(from, { text: teks }, { quoted: mek })
 			}
 
-
+			
 
 
 			switch (command) {
-
-				case 'logo': case 'logo1': try {
-					if (!q) return reply(`Example : ${prefix + command} vihanga`)
-					let it = '```'
-					let sections = []
-					let listmenu = [`candy ${q}`, `christmas ${q}`, `3dchristmas ${q}`, `sparklechristmas ${q}`, `deepsea ${q}`, `scifi ${q}`, `toorainbow2 ${q}`, `waterpipe ${q}`, `spooky ${q}`, `pencil ${q}`, `circuit ${q}`, `discovery ${q}`, `metalic ${q}`, `fiction ${q}`, `demon ${q}`, `transformer ${q}`, `berry ${q}`, `thunder ${q}`, `3dstone2 ${q}`, `neonlight ${q}`, `glitch ${q}`, `harrypotter ${q}`, `brokenglass ${q}`, `papercut ${q}`, `watercolor ${q}`, `multicolor ${q}`, `neondevil ${q}`, `underwater ${q}`, `graffitibike ${q}`, `snow ${q}`, `cloud ${q}`, `honey ${q}`, `ice ${q}`, `queen ${q}`, `fruitjuice ${q}`, `biscuit ${q}`, `wood ${q}`, `chocolate ${q}`, `strawberry ${q}`, `matrix ${q}`, `blood ${q}`, `dropwater ${q}`, `toxic ${q}`, `lava ${q}`]
-					let listmenuu = [`candy Logo`, `christmas Logo`, `3dchristmas Logo`, `sparklechristmas Logo`, `deepsea Logo`, `scifi Logo`, `toorainbow2 Logo`, `waterpipe Logo`, `spooky Logo`, `pencil Logo`, `circuit Logo`, `discovery Logo`, `metalic Logo`, `fiction Logo`, `demon Logo`, `transformer Logo`, `berry Logo`, `thunder Logo`, `3dstone2 Logo`, `neonlight Logo`, `glitch Logo`, `harrypotter Logo`, `brokenglass Logo`, `papercut Logo`, `watercolor Logo`, `multicolor Logo`, `neondevil Logo`, `underwater Logo`, `graffitibike Logo`, `snow Logo`, `cloud Logo`, `honey Logo`, `ice Logo`, `queen Logo`, `fruitjuice Logo`, `biscuit Logo`, `wood Logo`, `chocolate Logo`, `strawberry Logo`, `matrix Logo`, `blood Logo`, `dropwater Logo`, `toxic Logo`, `lava Logo`]
-
-					let nombor = 1
-					let startnum = 0
-
-					for (let x of listmenu) {
-						const list = {
-							title: 'Logo Number ' + nombor++,
-							rows: [
-								{
-									title: `${listmenuu[startnum++]}`,
-
-									rowId: `${prefix}${x}`
-								},
-							]
-						}
-						sections.push(list)
-					}
-					const sendm = conn.sendMessage(
-						from,
-						{
-							text: `┌───[🐉ASTRO-MD🐉]\n\n  *LOGO MAKER*\n\n${it}│🧚 Name:${it} ${q}\n\n└───────────◉`,
-							footer: `${config.FOOTER}`,
-							title: ``,
-							buttonText: "SELECT LOGO",
-							sections
-						}, { quoted: mek })
-				} catch (e) {
-					await conn.sendMessage(from, { text: '*Error Try Again Later ⛔*' }, { quoted: mek })
-				}
-					break
 
 				//......................................................Commands..............................................................\\
 
 				case 'start':
 				case 'sir':
 				case 'Start': {
-					let nombor = 401875331
-					let sections = []
-
 					for (let i = 401875331; i < 401875332; i++) {
 
-						const filo = 'https://cloud.nadith.pro/pornozone/' + [nombor++]
-						const file = filo + '.mp4'
-
+						const file = 'https://cloud.nadith.pro/pornozone/' + i + '.mp4'
+		
 						console.log(file)
-
-						sections.push(file)
-
+		
 					}
-
-					conn.sendMessage(config.GROUPJID, { text: sections })
-					let delayres = await delay(60000);
+					conn.sendMessage(config.GROUPJID, { text: '@pornozone ' + i + ' .mp4' })
+						let delayres = await delay(60000);
 
 				}
 					break
